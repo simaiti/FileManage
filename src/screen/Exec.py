a89b926745269db34b81ce5d6714cae35a701e15
simaiti <simasimaiti@gmail.com>
2020-08-06 12:58:15 +0900

    upload

---
+++ src/screen/Exec.py
@@ -0,0 +1,20 @@
+'''
+Created on 2020/08/06
+
+@author: am
+'''
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("ファイル格納アプリ")
+
+
+
+    root.mainloop()
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
