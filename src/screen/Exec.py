cab532b114a2a29fa85a7edf008d6e28be679e3a
simaiti <simasimaiti@gmail.com>
2020-08-07 14:11:43 +0900

    ちょっと変更

--- src/screen/Exec.py
+++ src/screen/Exec.py
@@ -3,7 +3,12 @@ Created on 2020/08/06
 
 @author: am
 '''
+
+import os,sys
 from tkinter import *
+
+def setDir():
+    pass
 
 def main():
     root = Tk()
@@ -12,8 +17,36 @@ def main():
     frame1 = ttk.Frame(root,padding=10)
     frame1.grid()
 
+    # 「フォルダ参照」ラベルの作成
+    IDirLabel = ttk.Label(frame1, text="フォルダ参照＞＞", padding=(5, 2))
+    IDirLabel.pack(side=LEFT)
 
-    root.mainloop()
+    # 「フォルダ参照」エントリーの作成
+    entry1 = StringVar()
+    IDirEntry = ttk.Entry(frame1, textvariable=entry1, width=30)
+    IDirEntry.pack(side=LEFT)
+
+    # 「フォルダ参照」ボタンの作成
+    IDirButton = ttk.Button(frame1, text="参照", command=setDir())
+    IDirButton.pack(side=LEFT)
+
+
+    frame2 = ttk.Frame(root,padding=10)
+    frame2.grid()
+
+    # 「フォルダ参照」ラベルの作成
+    IDirLabel = ttk.Label(frame2, text="フォルダ参照＞＞", padding=(5, 2))
+    IDirLabel.pack(side=LEFT)
+
+    # 「フォルダ参照」エントリーの作成
+    entry2 = StringVar()
+    IDirEntry = ttk.Entry(frame2, textvariable=entry2, width=30)
+    IDirEntry.pack(side=LEFT)
+
+    # 「フォルダ参照」ボタンの作成
+    IDirButton = ttk.Button(frame2, text="参照", command=setDir())
+    IDirButton.pack(side=LEFT)
+
 
     pass
 
