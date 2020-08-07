f081aed4825b5e1bc5269d1bc03dd3c1a147f7c0
simaiti <simasimaiti@gmail.com>
2020-08-07 14:32:40 +0900

    update

--- src/screen/Exec.py
+++ src/screen/Exec.py
@@ -6,8 +6,14 @@ Created on 2020/08/06
 
 import os,sys
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
 
-def setDir():
+def setDir(entry):
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    iDirPath = filedialog.askdirectory(initialdir = iDir)
+    entry.set(iDirPath)
     pass
 
 def main():
@@ -18,36 +24,36 @@ def main():
     frame1.grid()
 
     # 「フォルダ参照」ラベルの作成
-    IDirLabel = ttk.Label(frame1, text="フォルダ参照＞＞", padding=(5, 2))
-    IDirLabel.pack(side=LEFT)
+    IDirLabel1 = ttk.Label(frame1, text="フォルダ参照＞＞", padding=(5, 2))
+    IDirLabel1.pack(side=LEFT)
 
     # 「フォルダ参照」エントリーの作成
     entry1 = StringVar()
-    IDirEntry = ttk.Entry(frame1, textvariable=entry1, width=30)
-    IDirEntry.pack(side=LEFT)
+    IDirEntry1 = ttk.Entry(frame1, textvariable=entry1, width=30)
+    IDirEntry1.pack(side=LEFT)
 
     # 「フォルダ参照」ボタンの作成
-    IDirButton = ttk.Button(frame1, text="参照", command=setDir())
-    IDirButton.pack(side=LEFT)
+    IDirButton1 = ttk.Button(frame1, text="参照", command=setDir(entry1))
+    IDirButton1.pack(side=LEFT)
 
 
     frame2 = ttk.Frame(root,padding=10)
     frame2.grid()
 
     # 「フォルダ参照」ラベルの作成
-    IDirLabel = ttk.Label(frame2, text="フォルダ参照＞＞", padding=(5, 2))
-    IDirLabel.pack(side=LEFT)
+    IDirLabel2= ttk.Label(frame2, text="フォルダ参照＞＞", padding=(5, 2))
+    IDirLabel2.pack(side=LEFT)
 
     # 「フォルダ参照」エントリーの作成
     entry2 = StringVar()
-    IDirEntry = ttk.Entry(frame2, textvariable=entry2, width=30)
-    IDirEntry.pack(side=LEFT)
+    IDirEntry2 = ttk.Entry(frame2, textvariable=entry2, width=30)
+    IDirEntry2.pack(side=LEFT)
 
     # 「フォルダ参照」ボタンの作成
-    IDirButton = ttk.Button(frame2, text="参照", command=setDir())
-    IDirButton.pack(side=LEFT)
+    IDirButton2 = ttk.Button(frame2, text="参照", command=setDir(entry2))
+    IDirButton2.pack(side=LEFT)
 
-
+    root.mainloop()
     pass
 
 
